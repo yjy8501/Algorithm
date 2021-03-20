13b0415341c8e53453463266b7a1e9b90913146b
Yang, Jaeyong <yjy850146@gmail.com>
2021-03-20 16:23:50 +0900

    QuickSort 출력문 수정

--- src/QuickSort/QuickSort.java
+++ src/QuickSort/QuickSort.java
@@ -6,45 +6,44 @@ import java.util.Scanner;
 public class QuickSort {
 
 	public static String[] quickSort(String[] words, int p, int r) {
-		
+
 		// r과 p가 같으면 재귀호출을 끝낸다
 		if(r <= p) {
-			
+
 			// 배열을 리턴한다
 			return words;
 		}
-		
+
 		// 파티션 진입
 		int q = partition(words, p, r);
 
-
 		// 전위 퀵 sort
 		quickSort(words, p, q - 1);
-		
+
 		// 후위 퀵 sort
 		quickSort(words, q + 1, r);
-		
+
 		// 배열을 리턴한다
 		return words;
 
 	}
-	
+
 	// 파티션 메소드
 	public static int partition(String[] words, int p, int r) {
-		
+
 		// 피벗
 		String pivot = words[r];
-		
+
 		// 1구역 i
 		int i = p - 1 ;
-		
+
 		// 2구역 j
 		for (int j = p; j < r; j++) {
-			
-			// words[j] 와 pivot을 비교해서 words[j]가 더 작으면 
+
+			// words[j] 와 pivot을 비교해서 words[j]가 더 작으면
 			// words[++i] 과 words[j]를 교환
 			if (words[j].compareToIgnoreCase(pivot) <= 0) {
-				
+
 				String temp = words[++i];
 				words[i] = words[j];
 				words[j] = temp;
@@ -56,7 +55,7 @@ public class QuickSort {
 		String temp = words[i + 1];
 		words[i + 1] = words[r];
 		words[r] = temp;
-		
+
 		// pivot 위치 리턴
 		return i + 1;
 	}
@@ -74,7 +73,7 @@ public class QuickSort {
 
 		String words[] = new String[n];
 
-		System.out.print("10개의 단어 입력: ");
+		System.out.print(n+"개의 단어 입력: ");
 
 		for (int i = 0; i < words.length; i++) {
 			words[i] = sc.next();
@@ -85,10 +84,11 @@ public class QuickSort {
 
 		System.out.print("퀵 정렬 결과 = ");
 
+		for(int i = 0; i < words.length; i++) {
+			System.out.print(words[i]+" ");
+			}
 
-		  for(int i = 0; i < words.length; i++) { System.out.print(words[i]+" "); }
-
-
+		  sc.close();
 
 	}
 
